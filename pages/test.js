2fcca992bc1683300baa54d349c96e7eef10743b
erezrokah <erezrokah@users.noreply.github.com>
2021-01-25 15:05:39 +0100

    chore: next/image

--- pages/test.js
+++ pages/test.js
@@ -1,7 +1,10 @@
+import Image from "next/image";
+
 const TestPage = (props) => (
   <>
     <h1>getServerSideProps test:</h1>
     <pre>props: {JSON.stringify(props, null, 2)}</pre>
+    <Image src="/assets/netlify.png" alt="Netlify Logo" width={500} height={500} />
   </>
 );
 
