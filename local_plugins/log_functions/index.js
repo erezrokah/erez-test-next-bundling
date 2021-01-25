4eb3f7c81a4e1dd3cffd020666b2a681a1be6ffc
erezrokah <erezrokah@users.noreply.github.com>
2021-01-25 16:57:23 +0100

    chore: reproduce issue

--- local_plugins/log_functions/index.js
+++ local_plugins/log_functions/index.js
@@ -1,4 +1,17 @@
+const fs = require("fs/promises");
+
 module.exports = {
+  async onBuild({
+    constants,
+    utils: {
+      run: { command },
+    },
+  }) {
+    await fs.writeFile(
+      `${constants.FUNCTIONS_SRC}/test.js`,
+      `module.exports = require('jimp');`
+    );
+  },
   async onPostBuild({
     constants,
     utils: {
